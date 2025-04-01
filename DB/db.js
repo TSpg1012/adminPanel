dfa3e734120dc502a73770599a9f6a9bcbec824d
TSpg1012 <bd8g57ev2@code.edu.az>
2025-04-01 11:43:13 +0400

    modifed

--- DB/db.js
+++ DB/db.js
@@ -1,1 +1,12 @@
-console.log('hello');
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((error) => {
+    console.log("Error: " + error);
+  });
+
