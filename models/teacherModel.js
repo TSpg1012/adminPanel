9469a5cb499337326485ecd91facadfdd8a12348
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2025-04-03 11:36:01 +0400

    Create teacher model and controller

---
+++ models/teacherModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+const teacherSchema = mongoose.Schema(
+    {
+        id: Number,
+        name: String,
+        age: Number,
+        fincode: String,
+        mobile_number: Number,
+        password: String,
+    },
+    {
+        collection: "Teachers",
+    }
+);
+
+const Teacher = mongoose.model("Teacher",teacherSchema);
+
+module.exports = Teacher;
--- router/router.js
+++ router/router.js
@@ -1,11 +1,16 @@
 let router = require("express").Router();
 
 const studentController = require("../controller/studentController");
+const teacherController = require("../controller/teacherController");
 
 router.get("/students", studentController.getAllStudents);
 router.post("/students/add", studentController.addStudent);
 router.put("/students/update/:id", studentController.updateStudent);
 router.delete("/students/delete/:id", studentController.deleteStudentById);
-router.delete("/students/deleteMe", studentController.addStudent);
+
+router.get("/teachers", teacherController.getAllTeachers);
+router.post("/teachers/add", teacherController.addTeacher);
+router.put("/teachers/update/:id", teacherController.updateTeacher);
+router.delete("/teachers/delete/:id", teacherController.deleteTeacherById);
 
 module.exports = router;
